71bd97ba061ce704de9693315d1fa8c867acfcbf
Nhu Viet Nguyen <braveryandglory@gmail.com>
2025-11-30 13:23:07 +0100

    fix: add SessionProvider to wrap app for useSession hook

--- src/app/_components/providers.tsx
+++ src/app/_components/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -10,8 +11,10 @@ export function Providers({
   children: React.ReactNode;
 }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
